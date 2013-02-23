2369d323c34f506d786af676e2bc6fe1b63dc3c2
Midge 't Hoen <midgethoen@gmail.com>
2013-02-23 17:10:21 +0100

    Initial Commit

---
+++ circular shape test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  circular shape test
+//
+//  Created by Midge 't Hoen on 23-02-13.
+//  Copyright Midge 't Hoen 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
